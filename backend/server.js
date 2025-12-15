2273dba40dbf918f880695ce7cf339a9226176a5
Ankit-nehra <ankitnehra7000@gmail.com>
2025-12-15 22:35:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,7 @@ const io = new Server(server, {
   cors: {
     origin: "https://student-driver-frontend-cambr91nv-ankit-nehras-projects.vercel.app",
     methods: ["GET", "POST"],
+    credentials: true
   },
 });
 
@@ -120,5 +121,6 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
 );
+
 
 
